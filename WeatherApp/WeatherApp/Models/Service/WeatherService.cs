29078e725591b9c9c6e087f80c5e351812186b5a
Ishmael Sikhikhi <101715069+WebDeveloperDesigner2022@users.noreply.github.com>
2022-06-05 23:28:54 +0200

    Add project files.

---
+++ WeatherApp/WeatherApp/Models/Service/WeatherService.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace WeatherApp.Models.Service
+{
+    public class Weather
+    {
+        public int id { get; set; }
+        public string temperature { get; set; }
+
+        public string humidity { get; set; }
+        public string presure { get; set; }
+        public string cloud { get; set; }
+        public string wind { get; set; }
+    }
+}
---
+++ WeatherApp/WeatherApp/Models/WeatherService.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using SQLite;
+
+namespace WeatherApp.Models
+{
+    public class Weather
+    {
+        [PrimaryKey, AutoIncrement]
+        public int Id { get; set; }
+        public string Temperature { get; set; }
+
+        public string Humidity { get; set; }
+        public string Presure { get; set; }
+        public string Cloud { get; set; }
+        public string Wind { get; set; }
+    }
+}
