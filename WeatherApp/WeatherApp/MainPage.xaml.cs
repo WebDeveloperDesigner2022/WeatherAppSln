202112b86933f29d9fcefa7734580c90dc2ddf9c
WebDeveloperDesigner2022 <4275876@myuwc.ac.za>
2022-06-09 10:35:06 +0200

    Remove Database code

---
+++ WeatherApp/WeatherApp/MainPage.xaml.cs
@@ -0,0 +1,44 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+using WeatherApp.Models.Service;
+using Xamarin.Forms;
+
+namespace WeatherApp
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+
+            BindingContext = this;  
+        }
+
+        protected override async void OnAppearing()
+        {
+            base.OnAppearing();
+            await GetWeatherData();
+        }
+
+        public async Task<Root> GetWeatherData()
+        {
+
+            //var location = await Gcolocation.
+
+            HttpClient client = new HttpClient();
+            client.DefaultRequestHeaders.Add("Accept", "application/json");
+
+            string response = await client.GetStringAsync("https://api.openweathermap.org/data/2.5/weather?lat=35&lon=139&appid=8a26da812093f10b480c1f41fad89c2f");
+
+            Root temp = JsonConvert.DeserializeObject<Root>(response);
+
+            return temp;
+        }
+    }
+}
--- WeatherApp/WeatherApp/Services/WeatherService.cs
+++ WeatherApp/WeatherApp/Services/WeatherService.cs
@@ -13,16 +13,6 @@ namespace WeatherApp.Services
 {
     public class WeatherService
     {
-        public async Task<Root> GetRemoteTemp()
-        {
-            HttpClient client = new HttpClient();
-            client.DefaultRequestHeaders.Add("Accept", "application/json");
-
-            string response = await client.GetStringAsync("https://api.openweathermap.org/data/2.5/weather?lat=35&lon=139&appid=8a26da812093f10b480c1f41fad89c2f");
-
-            Root temp = JsonConvert.DeserializeObject<Root>(response);
-
-            return temp;
-        }
-}
+      
+    }
 }
